23879189b154e31a2d996be2bfa9ba09da441b34
mariussgal <marius.gal@esme.fr>
2025-04-26 19:50:49 +0200

    removed anchor

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 // next.config.mjs
 
-import autoCert from "anchor-pki/auto-cert/integrations/next";
-
-// If using .ts instead of .mjs, you can use the following comment to suppress the error
-// @ts-expect-error - No type definitions available for anchor-pki
-const withAutoCert = autoCert({
-  enabledEnv: "development",
-});
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withAutoCert(nextConfig);
+export default nextConfig;
