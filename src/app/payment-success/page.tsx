fcb413a0bdc8e610ff0cd7dc9d635401880accf2
mariussgal <marius.gal@esme.fr>
2025-04-26 19:52:43 +0200

    updated payment-success

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -1,20 +1,19 @@
-export default function PaymentSuccess({
-    searchParams,
-  }: {
-    searchParams: { [key: string]: string | string[] | undefined };
-  }) {
-    const amount = typeof searchParams.amount === 'string' ? searchParams.amount : '0.00';
-  
-    return (
-      <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
-        <div className="mb-10">
-          <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-          <h2 className="text-2xl">You successfully sent</h2>
-  
-          <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
-            ${amount}
-          </div>
+"use client";
+
+export default function PaymentSuccess() {
+  const params = new URLSearchParams(window.location.search);
+  const amount = params.get('amount') || '0.00';
+
+  return (
+    <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-blue-500 to-purple-500">
+      <div className="mb-10">
+        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
+        <h2 className="text-2xl">You successfully sent</h2>
+
+        <div className="bg-white p-2 rounded-md text-purple-500 mt-5 text-4xl font-bold">
+          ${amount}
         </div>
-      </main>
-    );
-  }
+      </div>
+    </main>
+  );
+}
